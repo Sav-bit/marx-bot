46ff98a339fe3d41f0332fdf1ed5e26ed29cf1bf
Sav <56634996+Sav-bit@users.noreply.github.com>
2020-09-09 13:03:42 +0200

    first commit

---
+++ bot.js
@@ -0,0 +1,38 @@
+const Telegraf = require('telegraf');
+const bot = new Telegraf('###telegraf-token-here###');
+
+const my = /my/gi;
+const mio = /(?<=(\W|^))((mio|tuo|suo|vostro)o*)(?=(\W|$))/gi;
+const mia = /(?<=(\W|^))((mia|tua|sua|vostra)a*)(?=(\W|$))/gi;
+const miei = /(?<=(\W|^))((miei|tuoi|suoi|vostri)i*)(?=(\W|$))/gi;
+const mie = /(?<=(\W|^))((mie|tue|sue|vostre)e*)(?=(\W|$))/gi;
+
+bot.start((ctx) => ctx.reply('Mi fa piacere essere qui compagni.'));
+
+
+bot.on('message', (ctx) => {
+  let msg = ctx.message.text;
+  let chat = ctx.chat.id;
+  try{
+  let noSpace = / /gi;
+  let simpsonref = msg.replace(noSpace,'');
+  //console.log(simpsonref);
+  if(simpsonref.toLowerCase().includes('unionesovietica?manonsieradisciolta?')) ctx.reply('Si, è questo che volevamo farvi credere *preme bottone*');
+  //english part
+  let nms = msg.replace(my,'OUR');
+
+  //italian part
+  nms = nms.replace(mio, 'NOSTRO');
+  nms = nms.replace(mia, 'NOSTRA');
+  nms = nms.replace(miei, 'NOSTRI');
+  nms = nms.replace(mie, 'NOSTRE');
+
+  if(nms.localeCompare(msg) != 0)
+
+  ctx.reply(nms+'*');
+  //send(chat, nms+"*");
+
+}catch(e){console.log(e)}
+});
+
+bot.launch();
